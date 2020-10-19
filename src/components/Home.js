ee53fba58df16aa83f4cbf8802fa3ff67360615c
HarishAshwinRaj <53622584+HarishAshwinRaj@users.noreply.github.com>
2020-10-19 18:16:32 +0530

    create-chat-screen chat screen is added and some responsiveness is added as well

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,10 +1,17 @@
 import React from "react";
 import ListContact from "./ListContact";
+import ChatComponent from "./ChatComponent";
+import useWindowDim from "../custHoos/usewindow";
 
 const Home = () => {
+  const { width, height } = useWindowDim();
+  console.log(width, "width");
   return (
-    <div style={{ flexDirection: "columns" }}>
-      <ListContact />
+    <div style={{ display: "flex", flexDirection: "row" }}>
+      <div style={{ width: width < 700 ? width : width / 3 }}>
+        <ListContact />
+      </div>
+      {width > 700 && <ChatComponent />}
     </div>
   );
 };
--- src/custHoos/usewindow.js
+++ src/custHoos/usewindow.js
@@ -1,28 +1,25 @@
 import { useState, useEffect } from "react";
 
-
 function getWindowDimensions() {
   const { innerWidth: width, innerHeight: height } = window;
+  //console.log(width, "width");
   return {
     width,
     height
   };
 }
 
-export function useWindowDim() {
-  const [windim,setwindim] = useState(getWindowDimensions());
-  useEffect(()=>{
-function setsize (){
-  setwindim(getWindowDimensions());
-}
-window.addEventListener('resize',setsize);
-return ()=>{
-  window.removeEventListener('resize',setsize);
-}
+export default function useWindowDim() {
+  const [windim, setwindim] = useState(getWindowDimensions());
+  useEffect(() => {
+    function setsize() {
+      setwindim(getWindowDimensions());
+    }
+    window.addEventListener("resize", setsize);
+    return () => {
+      window.removeEventListener("resize", setsize);
+    };
+  }, []);
 
-
-  }[]);
-
-return windim;
-
+  return windim;
 }
